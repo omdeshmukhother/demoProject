c2690a0c301c4f5191e72effb74977d4a0078709
omkar deshmukh <omdeshmukhother@gmail.com>
2018-10-28 13:11:04 +0530

    dgdfgdgf

--- src/test/java/com/example/demo/SpringRestWithPostgresApplicationTests.java
+++ src/test/java/com/example/demo/SpringRestWithPostgresApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import guru.springframework.SpringRestWithPostgresApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringRestWithPostgresApplication.class)
 public class SpringRestWithPostgresApplicationTests {
 
 	@Test
--- src/test/java/guru/springframework/SpringBootPostgreSQLApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootPostgreSQLApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringRestWithPostgresApplication.class)
+@SpringBootTest
 public class SpringBootPostgreSQLApplicationTests {
 
 	@Test
